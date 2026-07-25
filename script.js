ec08b006b7db00dc553816cb7d760741f5957ef7
sanchezanjana-coder <sanchezanjana@gmail.com>
2026-07-25 17:00:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// ===============================
+// NovaForge
+// Main Script
+// ===============================
 
+// Botón principal de la Landing
+const getStartedButtons = document.querySelectorAll(".primaryButton, .navButton");
+
+getStartedButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        window.location.href = "panel.html";
+
+    });
+
+});
